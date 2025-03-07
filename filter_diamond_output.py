ac22be3a8b60a9c1d20a809ea02513e0ade810bd
muhit-emon <82660145+muhit-emon@users.noreply.github.com>
2025-03-07 06:31:45 -0500

    Update filter_diamond_output.py

--- filter_diamond_output.py
+++ filter_diamond_output.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     filtered_diamond_output_file = output_fname + "_aln_filtered_" + gene_type + ".tsv"
 
     diamond_output = pd.read_csv(alignment_file, sep='\t', names=['qtitle','stitle', 'pident', 'bitscore', 'evalue', 'alignLen'], header=None)
-    diamond_output = diamond_output[diamond_output.bitscore >= 50] # bit-score filtering
-    #diamond_output = diamond_output[diamond_output.alignLen >= 100] # alignment length filtering
+    #diamond_output = diamond_output[diamond_output.bitscore >= 50] # bit-score filtering
+    diamond_output = diamond_output[diamond_output.alignLen >= 25] # alignment length filtering
 
     diamond_output.to_csv(filtered_diamond_output_file, sep='\t', index=False)
     
